0b05a9097ed3ba42bda0efeec0af63ba849e5f31
MERYEM KADA <129560502+Usermer@users.noreply.github.com>
2026-06-05 18:42:48 +0100

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        #time complexity O(n²)
+        # for liste in matrix:
+        #     return target in liste
+        # return -1
+        
+        #binary search time complexity O(nlogn)
+        for liste in matrix:
+            left,right=0,len(matrix[0])-1
+            while left<=right:
+                mid=(left+right)//2
+                if liste[mid]==target:
+                    return True
+                elif liste[mid]>target:
+                    right=mid-1
+                else:
+                    left=mid+1
+        return False
+
+        
